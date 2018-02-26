ec696db201bb347b5e407d631c527bdd12332e47
Galina Teplitskaya <mflucifer66x@gmail.com>
2018-02-26 17:54:54 +0300

    The one step

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig){
+  let counterOpenBrackets = 0;
+   for(let i =  0;i < str.length; i++){
+      for( let j =0; j<bracketsConfig.length; j++){
+          if (counterOpenBrackets < 0){
+              return false;
+          }
+         if( str[i] === bracketsConfig[j][0]){
+             counterOpenBrackets++;
+             }
+          if(str[i] === bracketsConfig[j][1]){
+              counterOpenBrackets = counterOpenBrackets - 1;
+         }
+      }
+   }
+   if (counterOpenBrackets === 0){
+       return true;
+   }
+   else {
+       return false;
+   }
 }
